fa63d6afa0fc433e322c45a69258b78c9eb11768
diaaahmed850 <diaaahmed850@gmail.com>
2019-05-02 16:06:17 +0200

    adding serializers for document model

--- BackEnd/visionapp/serializers.py
+++ BackEnd/visionapp/serializers.py
@@ -1,1 +1,19 @@
- 
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import documentModel,SocialUsers
+
+class UsersSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('password', 'email',)
+
+class documentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = documentModel
+        fields = '__all__'
+
+class createDocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = documentModel
+        fields = ('docType','data','title')
