d9ea8955516ffb09f60b9af1ad492aa72b4d6974
diaaahmed850 <diaaahmed850@gmail.com>
2019-05-02 15:58:15 +0200

    adding documnet model for saving user's documents

---
+++ BackEnd/visionapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
+import json
+
+types=[(0,'ID'),(1,'Passport'),(2,'Other')]
+providers = [('facebook', 'Facebook'), ('google', 'Google')]
+
+class jsonFieldHelper(JSONField):
+    def from_db_value(self,value,expression,connection,context):
+        if isinstance(value,str):
+            return json.loads(value)
+        return value
+
+class documentModel(models.Model):
+    title=models.CharField(default="other",max_length=255)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    docType=models.SmallIntegerField(default=2, choices=types)
+    data=jsonFieldHelper()
