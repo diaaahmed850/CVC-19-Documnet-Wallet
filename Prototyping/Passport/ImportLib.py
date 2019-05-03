6ffbd28fb70c5f291077efdeffb1364226bf6fd6
AbdulrahmanMahmoud13 <abdo.thedarkside@gmail.com>
2019-05-03 17:14:50 +0200

    Passport Notebook

---
+++ Prototyping/Passport/ImportLib.py
@@ -0,0 +1,23 @@
+import os
+import re
+import cv2
+import imutils
+import datetime
+import pytesseract
+import numpy as np
+import pandas as pd
+from PIL import Image
+from pylab import rcParams
+from autocorrect import spell
+from matplotlib import pyplot as plt
+from skimage.filters import threshold_local
+from imutils.object_detection import non_max_suppression
+
+
+
+
+
+
+
+
+
